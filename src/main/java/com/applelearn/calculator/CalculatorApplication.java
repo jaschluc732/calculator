773baa25b17fed33e048410c5871c9098dec9083
Jack Schluckbier <jaschluc@cisco.com>
2020-12-19 20:39:53 -0800

    adding source files

---
+++ src/main/java/com/applelearn/calculator/CalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.applelearn.calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorApplication.class, args);
+	}
+
+}
